4dfabb688cdcf9c8e09792ae8735b8ed8f0c4044
Sameer Ali <samali@umich.edu>
2019-03-19 15:43:42 -0400

    Various small changes

--- Project4_Battleship/Ship.cpp
+++ Project4_Battleship/Ship.cpp
@@ -4,12 +4,12 @@
  * Ship.cpp
  * Project UID 4cd2d66df3154b6c8b0ff27aa6791edb
  *
- * <#Name#>
- * <#Uniqname#>
+ * Sameer Ali & Yaozong Huang
+ * samali & yaozongh
  *
  * Project 4: Battleship
  *
- * <#description#>
+ * Ship implementation **FIXME**
  */
 
 #include <cmath>
@@ -18,7 +18,10 @@
 
 
 Ship::Ship() {
-    // TODO: write implementation here.
+    Position start;
+    Position end;
+    num_hits = 0;
+    size = 0;
 }
 
 Ship::Ship(Position start_in, Position end_in) {
