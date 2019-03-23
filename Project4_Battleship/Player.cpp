4de7212caa0b8698ab6291369cce4f163eb5e2f5
Yaozong Huang <yaozongh@umich.edu>
2019-03-23 19:15:38 -0400

    h

--- Project4_Battleship/Player.cpp
+++ Project4_Battleship/Player.cpp
@@ -125,23 +125,22 @@ void Player::attack(Player &opponent, Position pos) {
     if (opponent.position_not_hit(pos)) {
         for (int i = 0; i < opponent.num_ships; i++) {
             if (opponent.ships[i].has_position(pos)) {
+                int shipSize = opponent.ships[i].get_size();
                 opponent.grid[rowVal][colVal] = HIT_LETTER;
                 opponent_grid[rowVal][colVal] = HIT_LETTER;
                 opponent.ships[i].hit();
-                
                 if (opponent.ships[i].has_sunk()) {
                     opponent.remaining_ships -= 1;
-                    int shipSize = opponent.ships[i].get_size();
                     announce_ship_sunk(shipSize);
                 }
                 cout << name << " " << pos << " hit" << endl;
                 
                 return;
             }
+            opponent.grid[rowVal][colVal] = MISS_LETTER;
+            opponent_grid[rowVal][colVal] = MISS_LETTER;
+            cout << name << " " << pos << " miss" << endl;
         }
-        opponent.grid[rowVal][colVal] = MISS_LETTER;
-        opponent_grid[rowVal][colVal] = MISS_LETTER;
-        cout << name << " " << pos << " miss" << endl;
     }
     return;
 }
